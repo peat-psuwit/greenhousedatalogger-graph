dd7af6b47835ccdddac8d983c875fd1db6554326
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-04-18 20:12:21 +0700

    OptionBar: re-indent and remove Elevation.

--- src/Components/OptionBar.js
+++ src/Components/OptionBar.js
@@ -3,14 +3,11 @@ import {Toolbar,ToolbarRow,ToolbarSection,Elevation} from 'react-mdc-web/lib';
 
 export default class OptionBar extends Component {
 	render() {
-		return <Elevation z={4}>
-				<Toolbar fixed>
-				  <ToolbarRow>
-					<ToolbarSection align="start">
-					  
-					</ToolbarSection>
-				  </ToolbarRow>
-				</Toolbar>
-			</Elevation>	
+		return <Toolbar fixed>
+			<ToolbarRow>
+				<ToolbarSection align="start">
+				</ToolbarSection>
+			</ToolbarRow>
+		</Toolbar>
 	}
 }
