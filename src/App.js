7688f9023fc25bfa2901f47626fe60077bdbc1fc
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-09 16:55:24 +0700

    OptionBar: use information from filterStore to show dates.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <div className="App-intro">
-            <OptionBar />
+            <OptionBar {...this.props}/>
         </div>
       </div>
     );
--- src/Components/OptionBar.js
+++ src/Components/OptionBar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import {Toolbar,ToolbarRow,ToolbarSection,Button} from 'react-mdc-web/lib';
-import 'react-widgets/dist/css/react-widgets.css';
 import {DateField,DatePicker} from 'react-date-picker';
+import moment from 'moment';
+
+import Actions from '../Stores/Actions.js';
+
 import 'react-date-picker/index.css'
 
+moment.locale('th');
+
 export default class OptionBar extends Component {
+	onStartDateChanged(dateMoment) {
+		Actions.setFilter({
+			startDate: dateMoment
+		});
+	}
+
+	onEndDateChanged(dateMoment) {
+		Actions.setFilter({
+			endDate: dateMoment
+		});
+	}
+
 	render() {
 		return <Toolbar>
 			<ToolbarRow>
@@ -14,7 +31,8 @@ export default class OptionBar extends Component {
 						dateFormat="DD-MM-YYYY HH:mm:ss"
 						forceValidDate={true}
 						updateOnDateClick={true}
-						defaultValue={new Date()}
+						value={this.props.filter.startDate}
+						onChange={(dateString, { dateMoment, timestamp}) => {this.onStartDateChanged(dateMoment)}}
 						style={{
 							"color": "black",
 							"margin-left": "5px",
@@ -38,7 +56,8 @@ export default class OptionBar extends Component {
 						dateFormat="DD-MM-YYYY HH:mm:ss"
 						forceValidDate={true}
 						updateOnDateClick={true}
-						defaultValue={new Date()}
+						value={this.props.filter.endDate}
+						onChange={(dateString, { dateMoment, timestamp}) => {this.onEndDateChanged(dateMoment)}}
 						style={{
 							"color": "black",
 							"margin-left": "5px",
