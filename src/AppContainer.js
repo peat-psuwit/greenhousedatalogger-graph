a09b3ba6734a1e65ef5fb55e3c0e3b90390ef9e3
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-10 00:52:19 +0700

    AppContainer: add SensorStore

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -2,21 +2,27 @@ import React, {Component} from 'react';
 import {Container} from 'flux/utils';
 
 import App from './App.js';
+
 import filterStore from './Stores/FilterStore.js';
+import sensorStore from './Stores/SensorStore.js';
 
 class AppContainer extends Component {
   static getStores() {
-    return [filterStore];
+    return [filterStore, sensorStore];
   }
 
   static calculateState(prevState) {
     return {
       filter: filterStore.getState(),
+      sensors: sensorStore.getState(),
     };
   }
 
   render() {
-    return <App filter={this.state.filter} />;
+    return <App
+                filter={this.state.filter}
+                sensors={this.state.sensors}
+            />;
   }
 }
 
