a6e78a55ed9235899f712d39883af883fbaaddf2
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-09 20:57:57 +0700

    SensorList: indentation

--- src/Components/SensorList.js
+++ src/Components/SensorList.js
@@ -3,12 +3,12 @@ import {Grid} from 'react-mdc-web/lib';
 
 import SensorItem from './SensorItem.js';
 
-export default class SensorList extends Component {	
+export default class SensorList extends Component {
 	render() {
 		return <Grid>
 			{
 				this.props.sensors.map(function(sensor, i){
-				return <SensorItem sensorInfo={sensor} key={sensor.id} />
+					return <SensorItem sensorInfo={sensor} key={sensor.id} />
 				})
 			}
 		 </Grid>
