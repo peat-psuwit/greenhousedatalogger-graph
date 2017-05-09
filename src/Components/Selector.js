e11c0d4e6ad37e5b863683065c26fe8a571cbc92
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-10 01:23:42 +0700

    Selector/SensorList: hook up with the SensorStore

--- src/Components/Selector.js
+++ src/Components/Selector.js
@@ -5,17 +5,11 @@ import OptionBar from './OptionBar.js';
 import SensorList from './SensorList.js';
 
 export default function Selector(props) {
-    var sensors = [{
-        id: "GH001"
-    },{
-        id: "GH002"
-    }];
-
     return (
         <Elevation z={4}>
             <OptionBar {...props} />
 
-            <SensorList sensors={sensors} />
+            <SensorList {...props} />
         </Elevation>
     );
 }
--- src/Components/SensorList.js
+++ src/Components/SensorList.js
@@ -5,12 +5,31 @@ import SensorItem from './SensorItem.js';
 
 export default class SensorList extends Component {
 	render() {
-		return <Grid>
-			{
-				this.props.sensors.map(function(sensor, i){
-					return <SensorItem sensorInfo={sensor} key={sensor.id} />
-				})
-			}
-		 </Grid>
+		var sensors = this.props.sensors;
+
+		if (sensors.error) {
+			//TODO: proper error
+			return <div>Error!</div>
+		}
+
+		return (
+			<div>
+				<Grid>
+				{
+					sensors.sensors.map(function(sensor, i){
+						return <SensorItem sensorInfo={sensor} key={sensor.id} />
+					}).toJS()
+				}
+				</Grid>
+
+				{function() {
+					if (sensors.loading)
+						//TODO: proper spinner
+						return <div>Loading</div>
+					else
+						return <div></div>
+				} () }
+			</div>
+		);
 	}
 }
