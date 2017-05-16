6f3a6081f9aca5937a6191cf7c36d971f59bae68
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-16 16:53:04 +0700

    Grapher: move getDataSet() to the class

--- src/Components/Grapher.js
+++ src/Components/Grapher.js
@@ -2,41 +2,41 @@ import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
 import randomColor from 'randomcolor';
 
-function getDataSet(sensorData, filter) {
-    if (!filter.getStartDate() || !filter.getEndDate())
-        return [];
-
-    return sensorData.toSeq()
-        .filter((value, key) => filter.getSensors().has(key))
-        .map(function(currentSensorData, sensorID) {
-            var data = currentSensorData.toSeq()
-                .filter((value, key) =>
-                    (
-                        (
-                            filter.getStartDate().isBefore(value.timestamp) ||
-                            filter.getStartDate().isSame(value.timestamp)
-                        ) && (
-                            filter.getEndDate().isAfter(value.timestamp) ||
-                            filter.getEndDate().isSame(value.timestamp)
-                        )
-                    )
-                ).map((value, key) => ({
-                    x: value.timestamp,
-                    y: value[filter.getSelectedField()]
-                })).toJS();
-
-            return {
-                label: sensorID,
-                borderColor: randomColor(), //TODO: cache color
-                data: data
-            };
-        }).toArray();
-}
-
 export default class Grapher extends Component {
+    getDataSet(sensorData, filter) {
+        if (!filter.getStartDate() || !filter.getEndDate())
+            return [];
+
+        return sensorData.toSeq()
+            .filter((value, key) => filter.getSensors().has(key))
+            .map(function(currentSensorData, sensorID) {
+                var data = currentSensorData.toSeq()
+                    .filter((value, key) =>
+                        (
+                            (
+                                filter.getStartDate().isBefore(value.timestamp) ||
+                                filter.getStartDate().isSame(value.timestamp)
+                            ) && (
+                                filter.getEndDate().isAfter(value.timestamp) ||
+                                filter.getEndDate().isSame(value.timestamp)
+                            )
+                        )
+                    ).map((value, key) => ({
+                        x: value.timestamp,
+                        y: value[filter.getSelectedField()]
+                    })).toJS();
+
+                return {
+                    label: sensorID,
+                    borderColor: randomColor(), //TODO: cache color
+                    data: data
+                };
+            }).toArray();
+    }
+
     render() {
         var data = {
-            datasets: getDataSet(
+            datasets: this.getDataSet(
                 this.props.sensorData.getValue(),
                 this.props.filter
             )
