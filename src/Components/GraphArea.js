3353801fd8201bdc2a710eac6b288e724486bbcc
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-21 13:35:38 +0700

    GraphArea: add GraphOverlay and required css.

--- src/Components/GraphArea.js
+++ src/Components/GraphArea.js
@@ -3,13 +3,18 @@ import { Elevation } from 'react-mdc-web/lib';
 
 import FieldSelectorBar from './FieldSelectorBar.js';
 import Grapher from './Grapher.js';
+import GraphOverlay from './GraphOverlay.js';
+
+import '../css/GraphArea.css';
 
 export default function GraphArea(props) {
     return (
-        <Elevation z={4}>
+        <Elevation z={4} className="graph-area">
             <FieldSelectorBar {...props} />
 
             <Grapher {...props} />
+
+            <GraphOverlay {...props} />
         </Elevation>
     );
 }
