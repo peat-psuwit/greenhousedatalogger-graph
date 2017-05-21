bc2f9b6842d85a333fc4bc78c344a62d027c629e
Ratchanan Srirattanamet <peathot@hotmail.com>
2017-05-21 16:39:50 +0700

    TitleBar: put a z-index fix.

--- src/Components/TitleBar.js
+++ src/Components/TitleBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import {Toolbar,ToolbarRow,ToolbarSection,ToolbarTitle} from 'react-mdc-web/lib';
 
+import '../css/TitleBar.css'
+
 export default class TitleBar extends Component {
 	render() {
-		return <Toolbar fixed>
+		return <Toolbar fixed className="titlebar">
 				  <ToolbarRow>
 					<ToolbarSection align="start">
 					  <ToolbarTitle>Greenhouse Data Grapher</ToolbarTitle>
